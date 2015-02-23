f26fe5ac6e7f4cf6b9eaf416c08d544e0e3735f5
gilshoji <gilshoji@yahoo.com.br>
2015-02-23 11:02:50 -0300

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -3,76 +3,77 @@ library(plyr)
 
 # Step 1 - Merges the training and the test sets to create one data set.
 
+# read feature dataset
+features <- read.table("C:/Gilson/coursera/Getting_Cleaning_Data/Project/UCI HAR Dataset/features.txt")
+
 # reading train datasets
-x_train <- read.table("C:/GILSON/coursera/data cleaning/bases/Project/UCI HAR Dataset/train/X_train.txt")
-y_train <- read.table("C:/GILSON/coursera/data cleaning/bases/Project/UCI HAR Dataset/train/y_train.txt")
-subject_train <- read.table("C:/GILSON/coursera/data cleaning/bases/Project/UCI HAR Dataset/train/subject_train.txt")
- 
+x_train <- read.table("C:/Gilson/coursera/Getting_Cleaning_Data/Project/UCI HAR Dataset/train/X_train.txt")
+y_train <- read.table("C:/Gilson/coursera/Getting_Cleaning_Data/Project/UCI HAR Dataset/train/y_train.txt")
+subject_train <- read.table("C:/Gilson/coursera/Getting_Cleaning_Data/Project/UCI HAR Dataset/train/subject_train.txt")
+
+# Assign column names
+colnames(subject_train)  = "subjectId";
+colnames(x_train)        = features[,2]; 
+colnames(y_train)        = "activityId";
+
 # create a train dataset
-train <- cbind(x_train,y_train,subject_train)
+train <- cbind(y_train,subject_train,x_train)
 
 # reading test data sets
-x_test <- read.table("C:/GILSON/coursera/data cleaning/bases/Project/UCI HAR Dataset/train/X_test.txt")
-y_test <- read.table("C:/GILSON/coursera/data cleaning/bases/Project/UCI HAR Dataset/train/y_test.txt")
-subject_test <- read.table("C:/GILSON/coursera/data cleaning/bases/Project/UCI HAR Dataset/train/subject_test.txt")
+x_test <- read.table("C:/Gilson/coursera/Getting_Cleaning_Data/Project/UCI HAR Dataset/test/X_test.txt")
+y_test <- read.table("C:/Gilson/coursera/Getting_Cleaning_Data/Project/UCI HAR Dataset/test/y_test.txt")
+subject_test <- read.table("C:/Gilson/coursera/Getting_Cleaning_Data/Project/UCI HAR Dataset/test/subject_test.txt")
+
+# Assign column names
+colnames(subject_test)  = "subjectId";
+colnames(x_test)        = features[,2]; 
+colnames(y_test)        = "activityId";
 
 # create a train dataset
-test <- cbind(x_test,y_test,subject_test)
+test <- cbind(y_test,subject_test,x_test)
 
 # create a complete data
 finaldata = rbind(train,test);
 
 # Step 2 - Extracts only the measurements on the mean and standard deviation for each measurement. 
 
-# read feature dataset
-features <- read.table("C:/GILSON/coursera/data cleaning/bases/Project/UCI HAR Dataset/train/features.txt")
-
 # find columns with mean or std in their names
 find_cols <- grep("-(mean|std)\\(\\)", features[, 2])
 
 # create a subset with this columns
-finaldata <- finaldata[, find_cols]
+Finaldata <- finaldata[, find_cols]
 
 # Make the label columns
-names(finaldata) <- features[find_cols, 2]
+names(Finaldata) <- features[find_cols, 2]
 
 # Step 3 - Use descriptive activity names to name the activities in the data set
 
 # read the activity table
-activity <- read.table("C:/GILSON/coursera/data cleaning/bases/Project/UCI HAR Dataset/train/activity_labels.txt")
-
-# Merge the finalData with the acitivity table
-finalData = merge(finalData,activity,by='activityId',all.x=TRUE);
-
-# correct column name
-colnames  = colnames(finalData); 
+activity <- read.table("C:/Gilson/coursera/Getting_Cleaning_Data/Project/UCI HAR Dataset/activity_labels.txt",header=FALSE)
+colnames(activity)  = c('activityId','activityType');
 
 # Step 4 - Appropriately label the data set with descriptive variable names
 
-for (i in 1:length(colNames)) 
-{
-  colNames[i] = gsub("\\()","",colNames[i])
-  colNames[i] = gsub("-std$","StandardDeviation",colNames[i])
-  colNames[i] = gsub("-mean","Mean",colNames[i])
-  colNames[i] = gsub("^(t)","Time",colNames[i])
-  colNames[i] = gsub("^(f)","Frequency",colNames[i])
-  colNames[i] = gsub("AccMag","AccMagnitude",colNames[i])
-  colNames[i] = gsub("([Bb]odyaccjerkmag)","BodyAccJerkMagnitude",colNames[i])
-  colNames[i] = gsub("JerkMag","JerkMagnitude",colNames[i])
-  colNames[i] = gsub("GyroMag","GyroMagnitude",colNames[i])
-};
+  names(Finaldata) = gsub("\\()","",names(Finaldata))
+  names(Finaldata) = gsub("-std$","StandardDeviation",names(Finaldata))
+  names(Finaldata) = gsub("-mean","Mean",names(Finaldata))
+  names(Finaldata) = gsub("^(t)","Time",names(Finaldata))
+  names(Finaldata) = gsub("^(f)","Frequency",names(Finaldata))
+  names(Finaldata) = gsub("AccMag","AccMagnitude",names(Finaldata))
+  names(Finaldata) = gsub("([Bb]odyaccjerkmag)","BodyAccJerkMagnitude",names(Finaldata))
+  names(Finaldata) = gsub("JerkMag","JerkMagnitude",names(Finaldata))
+  names(Finaldata) = gsub("GyroMag","GyroMagnitude",names(Finaldata))
+
 
 # Step 5 - From the data set in step 4, create a second, independent tidy data set with the average of each variable
 # for each activity and each subject
 
-# Create finalData2 without the activityType column
-final2  = finalData[,names(finalData) != 'activityType'];
+Finaldata = merge(Finaldata,activity,by='activityId',all.x=TRUE);
 
-# Summarizing the finalData2 table to include the mean of each variable for each activity and each subject
-Final_Data    = aggregate(final2[,names(final2) != c('activityId','subjectId')],by=list(activityId=finalDataN2$activityId,subjectId = finalData2$subjectId),mean);
+# Create a new table, Finaldata without the activityId column
+Finaldata  = Finaldata[,names(Finaldata) != 'activityId'];
 
-# Merging the Final_Data with activity
-Final_Data    = merge(Final_Data,activity,by='activityId',all.x=TRUE);
+tidy = ddply(Finaldata, c("subjectId","activityType"), numcolwise(mean))
 
 # Export the Final_Data 
-write.table(Final_Data, "C:/GILSON/coursera/data cleaning/bases/Project/UCI HAR Dataset/train/Final_Data.txt'"row.name=FALSE,sep='\t');
+write.table(Finaldata, 'C:/Gilson/coursera/Getting_Cleaning_Data/Project/UCI HAR Dataset/Final_Data.txt',row.name=FALSE,sep='\t');
